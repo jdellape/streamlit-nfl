3fb6fe9f17cdc29046192c053614802a6f946402
John Dellape <dellapejw@gmail.com>
2022-02-27 15:00:23 -0500

    test v1 with qb data

--- app.py
+++ app.py
@@ -1,86 +1,67 @@
 import streamlit as st
+import requests
 import pandas as pd
-import base64
-#import matplotlib.pyplot as plt
-import numpy as np
 import altair as alt
 
-st.title('NFL Football Stats (Rushing) Explorer')
+st.title('Fantasy Football Data Explorer')
 
 st.markdown("""
-This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+This app helps fantasy football owners make roster decisions by graphing historical player stats
+* Use the side bar to the left to dynamically filter for players and statistics of interest.
 * **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
 """)
 
-st.sidebar.header('User Input Features')
+REGULAR_QB_STAT_OPTIONS = ['TD', 'YDS']
+ADVANCED_QB_STAT_OPTIONS = ['BadTh', 'Prss']
+STAT_OPTIONS_DROP_DOWN = []
+
+st.sidebar.header('Filter Dataset By: ')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(2019,2022))))
+selected_position = st.sidebar.selectbox('Position',  ['QB','RB','WR','TE'])
+selected_stat_category = st.sidebar.selectbox('Stat Category',  ['advanced','regular'])
+if selected_stat_category == 'advanced':
+    STAT_OPTIONS_DROP_DOWN = ADVANCED_QB_STAT_OPTIONS
+else:
+    STAT_OPTIONS_DROP_DOWN = REGULAR_QB_STAT_OPTIONS
+selected_stats = st.sidebar.selectbox('Stat To Present', STAT_OPTIONS_DROP_DOWN)
 
-# Web scraping of NFL player stats
-# https://www.pro-football-reference.com/years/2019/rushing.htm
+
+# Load up json data I have hosted on github
 @st.cache
-def load_normal_data(year):
-    url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
-    html = pd.read_html(url, header = 1)
-    df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-    raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis=1)
-    playerstats['Yds'] = pd.to_numeric(playerstats['Yds'])
-    playerstats['TD'] = pd.to_numeric(playerstats['TD'])
-    return playerstats
+def load_json_data():
+    url = 'https://raw.githubusercontent.com/jdellape/data-sources/main/nfl/qb/season_summaries/qb_summary.json'
+    r = requests.get(url)
+    return r.json()
 
-adv_rush_df = pd.read_csv("https://raw.githubusercontent.com/jdellape/data-sources/main/nfl/advanced_rushing_2021.csv")
-st.write(adv_rush_df)
+#Capture and filter stat data
+tuple_list_to_analyze = []
+test_json = load_json_data()
 
-#Simply adding to fetch advanced stats table example
-# @st.cache
-# def load_advanced_data(year):
-#     url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing_advanced.htm"
-#     html = pd.read_html(url, header = 1)
-#     df = html[0]
+data_set_to_analyze = list(filter(lambda d: d['category'] == selected_stat_category and d['year'] == str(selected_year), test_json))
 
-#     raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-#     raw = raw.fillna(0)
-#     playerstats = raw.drop(['Rk'], axis=1)
-#     return playerstats
+for row in data_set_to_analyze:
+    try:
+        tuple_list_to_analyze.append((row['player_id'], row['TD_overall'], row['Yds_overall']))
+    except:
+        pass
 
-playerstats = load_normal_data(selected_year)
-# player_adv_stats = load_advanced_data(selected_year)
-#Write advanced stats out to show verify I have captured the table
-#st.dataframe(player_adv_stats)
+df_qbs = pd.DataFrame(tuple_list_to_analyze, columns =['ID', 'TD', 'Yds'])
+df_qbs['Yds'] = pd.to_numeric(df_qbs['Yds'])
+df_qbs['TD'] = pd.to_numeric(df_qbs['TD'])
 
-# Sidebar - Team selection
-sorted_unique_team = sorted(playerstats.Tm.unique())
-selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+st.header('Write out Raw json file containing stats filered by user input')
+st.write(data_set_to_analyze)
 
-# Sidebar - Position selection
-unique_pos = ['RB','QB','WR','FB','TE']
-selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
-
-# Filtering data
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
-
-st.header('Display Player Stats of Selected Team(s)')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-st.dataframe(df_selected_team)
-
-
+st.header('Test a scatter plot based upon QB Data. Hard Coded to Only Present yards by TDs')
+st.markdown("""
+Must select "regular" from Stat Category drop down in order for this to display while under development.
+""")
+#Test a scatter plot with my json data
 #Convert columns to integer for testing out a chart
-scatter = alt.Chart(playerstats[['Yds','TD', 'Player']]).mark_circle(size=60).encode(
+qb_scatter = alt.Chart(df_qbs).mark_circle(size=60).encode(
     x='Yds',
     y='TD',
-    tooltip='Player'
+    tooltip='ID'
 ).interactive()
 
-st.altair_chart(scatter, use_container_width=True)
-
-# Download NBA player stats data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
-    return href
-
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+st.altair_chart(qb_scatter, use_container_width=True)
