cfb3f646d6adfefe45e70cf20bf39db35bfc982a
John Dellape <dellapejw@gmail.com>
2022-03-25 07:29:17 -0400

    dynamic agg method enhancement
    Allowing user to select how they want to aggregate yearly data and some ui cleanup

--- app.py
+++ app.py
@@ -14,39 +14,10 @@ This app helps fantasy football owners make roster decisions by graphing histori
 * **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
 """)
 
-SCORING_TYPES = ['Normal','PPR', 'Halfpoint PPR', 'DraftKings','FanDuel']
-
-SCORING_TYPE_COL_MATCHING = {'Normal':'FantPt', 'PPR':'PPR', 'Halfpoint PPR':'HalfpointPPR', 'DraftKings':'DKPt', 'FanDuel':'FDPt'}
-
-SCORING_TYPE_TOOL_TIP = {'Normal':'trad style<br/>  blah blah', 'PPR':'PPR style', 'Halfpoint PPR':'Halfpoint PPR', 'DraftKings':'Draft Kings Style', 'FanDuel':'Fan Duel Style'}
-
-SELECTION_DETAILS_COLS = {'Normal':['FantPt','FantPtpG','Player','year'], 
-                          'PPR':['PPR','PPRpG','Player','year'],
-                          'Halfpoint PPR':['HalfpointPPR','HalfpointPPRpG','Player','year'],
-                          'DraftKings':['DKPt','DKPtpG','Player','year'], 
-                          'FanDuel':['FDPt','FDPtpG','Player','year']}
-
-st.sidebar.header('Filter Stripplot By: ')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(2019,2022))))
-
-#Allow user to enter a point floor
-point_value_floor = st.sidebar.number_input('Point Floor ', value=0, min_value=0)
-
-st.sidebar.header('Set Stripplot y axis:')
-selected_scoring_type = st.sidebar.selectbox('Fantasy Point Scoring Style', SCORING_TYPES)
-
-with st.sidebar.expander("See scoring style details"):
-     st.write('feature under construction')
-
-#Allow choice of analysis by total season points OR average points per game
-selected_scoring_agg = st.sidebar.radio(
-     "View Points by",
-     ('Average Per Game', 'Season Total'))
-
+# Load up data I have hosted on github
 DATA_URL = 'https://raw.githubusercontent.com/jdellape/data-sources/main/nfl/fantasy_points_by_player_by_year.csv'
 WEEKLY_DATA_URL = 'https://raw.githubusercontent.com/jdellape/data-sources/main/nfl/fantasy_weekly_results_by_player.csv'
 
-# Load up data I have hosted on github
 @st.cache
 def load_data(url):
     data = pd.read_csv(url)
@@ -60,8 +31,72 @@ data_load_state = st.text('Loading weekly fantasy data...')
 weekly_data = load_data(WEEKLY_DATA_URL)
 data_load_state.text('All data loaded!')
 
+#Write out raw yearly summary to ui
 st.header('Raw .csv data containing yearly stats')
 st.write(data)
+
+
+SCORING_TYPES = ['Normal','PPR', 'Halfpoint PPR', 'DraftKings','FanDuel']
+
+SCORING_TYPE_COL_MATCHING = {'Normal':'FantPt', 'PPR':'PPR', 'Halfpoint PPR':'HalfpointPPR', 'DraftKings':'DKPt', 'FanDuel':'FDPt'}
+
+SCORING_TYPE_TOOL_TIP = {'Normal':'trad style<br/>  blah blah', 'PPR':'PPR style', 'Halfpoint PPR':'Halfpoint PPR', 'DraftKings':'Draft Kings Style', 'FanDuel':'Fan Duel Style'}
+
+SELECTION_DETAILS_COLS = {'Normal':['FantPt','FantPtpG','Player','year'], 
+                          'PPR':['PPR','PPRpG','Player','year'],
+                          'Halfpoint PPR':['HalfpointPPR','HalfpointPPRpG','Player','year'],
+                          'DraftKings':['DKPt','DKPtpG','Player','year'], 
+                          'FanDuel':['FDPt','FDPtpG','Player','year']}
+
+def get_pivot_table(df, start, end, agg_func, scoring_type):
+    """Function to get yearly player data to plot on stripplot based on user input
+       Shout out to mito for making this way easier to do: https://www.trymito.io/ 
+    """
+    #Filter dataframe according to years specified by user
+    df = df[df.year.isin(range(int(start), int(end) + 1))]
+    #Drop columns I don't need
+    unused_columns = df.columns.difference(set(['Player','FantPos']).union(set([])).union(set({SCORING_TYPE_COL_MATCHING[scoring_type]})))
+    tmp_df = df.drop(unused_columns, axis=1)
+    #Compile pivot table df
+    pivot_table = tmp_df.pivot_table(
+        index=['Player','FantPos'],
+        values=[SCORING_TYPE_COL_MATCHING[scoring_type]],
+        aggfunc={SCORING_TYPE_COL_MATCHING[scoring_type]: [agg_func]}
+    )
+    #Formatting stuff
+    pivot_table.set_axis([col for col in pivot_table.keys()], axis=1, inplace=True)
+    fantasy_points_by_year_pivot = pivot_table.reset_index()
+    fantasy_points_by_year_pivot.columns = ['Player', 'FantPos', SCORING_TYPE_COL_MATCHING[scoring_type]]
+    #round last column if needed
+    if agg_func != 'sum':
+        fantasy_points_by_year_pivot[SCORING_TYPE_COL_MATCHING[scoring_type]] = fantasy_points_by_year_pivot[SCORING_TYPE_COL_MATCHING[scoring_type]].round(1)
+    return fantasy_points_by_year_pivot
+
+st.sidebar.header('Season Total Data Settings')
+
+start_year, end_year = st.sidebar.select_slider(
+     'Range of Years to Include',
+     options=list(range(2019,2022)),
+     value=(2019,2021))
+
+selected_agg_func = st.sidebar.radio('Aggregation Method to Apply',['sum','mean','median'])
+
+#Allow user to enter a point floor
+point_value_floor = st.sidebar.number_input('Point Floor for Chart Display', value=0, min_value=0)
+
+selected_scoring_type = st.sidebar.selectbox('Fantasy Point Scoring Style', SCORING_TYPES)
+
+with st.sidebar.expander("See scoring style details"):
+     st.write('feature under construction')
+
+#Allow choice of analysis by total season points OR average points per game
+st.sidebar.header('THIS CURRENTLY NOT DOING ANYTHING. NEED TO DISCUSS OPTIONS TO NORMALIZE.')
+selected_scoring_agg = st.sidebar.radio(
+     "View Points by",
+     ('Average Per Game', 'Season Total'))
+
+#Get a df for charting season totals data based upon user entry
+pivot_df = get_pivot_table(data, start_year, end_year, selected_agg_func, selected_scoring_type)
 
 st.header('Interactive Stripplot')
 st.markdown("""
@@ -74,15 +109,17 @@ with st.expander('Players Already Drafted'):
     players_already_drafted = st.multiselect('Indicate Players Already Taken',
         distinct_player_names)
 
-def get_y():
-    "Helper function to get the appropriate score column to display on stripplot y axis"
-    if selected_scoring_agg == 'Average Per Game':
-        return SCORING_TYPE_COL_MATCHING[selected_scoring_type] + 'pG'
-    else:
-        return SCORING_TYPE_COL_MATCHING[selected_scoring_type]
+#Don't think I need this currently
+# def get_y():
+#     "Helper function to get the appropriate score column to display on stripplot y axis"
+#     if selected_scoring_agg == 'Average Per Game':
+#         return SCORING_TYPE_COL_MATCHING[selected_scoring_type] + 'pG'
+#     else:
+#         return SCORING_TYPE_COL_MATCHING[selected_scoring_type]
 
 #Get user selected y value
-selected_y = get_y()
+#selected_y = get_y()
+selected_y = SCORING_TYPE_COL_MATCHING[selected_scoring_type]
 
 #Construct altair plot to visualize data
 
@@ -91,10 +128,10 @@ strip_plot_selector = alt.selection_multi(empty='all', fields=['Player'])
 
 #Create a stripplot
 #Get the slice of your dataframe to plot
-stripplot_data = data[(data['year'] == selected_year) & (data['Player'].isin(players_already_drafted) == False) & (data[selected_y] >= point_value_floor)]
+stripplot_data = pivot_df[(pivot_df['Player'].isin(players_already_drafted) == False) & (pivot_df[selected_y] >= point_value_floor)]
 
 #Get values to use as the min and max values for the y-axis
-stripplot_y_max = stripplot_data[selected_y].max() + 1
+stripplot_y_max = stripplot_data[selected_y].max() + 10
 
 stripplot_y_min = point_value_floor
 
@@ -112,7 +149,7 @@ stripplot =  alt.Chart(stripplot_data).mark_circle(size=50).encode(
                 axis=alt.Axis(title=None)),
     color=alt.condition(strip_plot_selector, 'FantPos', alt.value('lightgray'), legend=None),
     #color=alt.condition(interval, 'Origin', alt.value('lightgray')
-    tooltip='Player',
+    tooltip=['Player', selected_y],
     column=alt.Column(
         'FantPos:N',
         header=alt.Header(
@@ -163,7 +200,7 @@ line = alt.Chart(data).mark_line(
 #Write out the charts to streamlit app
 st.altair_chart(stripplot & line)
 
-#Test out layered density chart
+#Create layered density chart for detailed player comparison
 st.header('Fantasy Point Density Estimate Chart using Weekly Points Scored')
 st.markdown("""
 Select Players from the dropdown below to get a detailed visualization of how their weekly fantasy point distribution compares.
@@ -179,9 +216,6 @@ data_to_chart = data_to_chart[data_to_chart['fantasy_table_column']=='FantPt']
 unique_selected_ids = list(data_to_chart.player_id.unique()) 
 
 img_urls = [f'https://www.pro-football-reference.com/req/20180910/images/headshots/{id.split("/")[1]}_2021.jpg' for id in unique_selected_ids]
-
-# st.write('You selected:')
-
 
 extent_max = data_to_chart.value.max() + 1
 
