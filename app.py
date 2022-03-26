b7634376a26036b7f3c1d61110e84edb26b28fd1
John Dellape <dellapejw@gmail.com>
2022-03-26 12:07:07 -0400

    add normalize checkbox

--- app.py
+++ app.py
@@ -48,30 +48,37 @@ SELECTION_DETAILS_COLS = {'Normal':['FantPt','FantPtpG','Player','year'],
                           'DraftKings':['DKPt','DKPtpG','Player','year'], 
                           'FanDuel':['FDPt','FDPtpG','Player','year']}
 
-def get_pivot_table(df, start, end, agg_func, scoring_type):
+def get_y(scoring_type, normalize_check):
+    "Helper function to get the appropriate score column to display on stripplot y axis"
+    if normalize_check:
+        return SCORING_TYPE_COL_MATCHING[scoring_type] + 'pG'
+    return SCORING_TYPE_COL_MATCHING[scoring_type]
+
+def get_pivot_table(df, start, end, agg_func, col_to_agg_name):
     """Function to get yearly player data to plot on stripplot based on user input
        Shout out to mito for making this way easier to do: https://www.trymito.io/ 
     """
     #Filter dataframe according to years specified by user
     df = df[df.year.isin(range(int(start), int(end) + 1))]
     #Drop columns I don't need
-    unused_columns = df.columns.difference(set(['Player','FantPos']).union(set([])).union(set({SCORING_TYPE_COL_MATCHING[scoring_type]})))
+    unused_columns = df.columns.difference(set(['Player','FantPos']).union(set([])).union(set({col_to_agg_name})))
     tmp_df = df.drop(unused_columns, axis=1)
     #Compile pivot table df
     pivot_table = tmp_df.pivot_table(
         index=['Player','FantPos'],
-        values=[SCORING_TYPE_COL_MATCHING[scoring_type]],
-        aggfunc={SCORING_TYPE_COL_MATCHING[scoring_type]: [agg_func]}
+        values=[col_to_agg_name],
+        aggfunc={col_to_agg_name: [agg_func]}
     )
     #Formatting stuff
     pivot_table.set_axis([col for col in pivot_table.keys()], axis=1, inplace=True)
     fantasy_points_by_year_pivot = pivot_table.reset_index()
-    fantasy_points_by_year_pivot.columns = ['Player', 'FantPos', SCORING_TYPE_COL_MATCHING[scoring_type]]
+    fantasy_points_by_year_pivot.columns = ['Player', 'FantPos', col_to_agg_name]
     #round last column if needed
     if agg_func != 'sum':
-        fantasy_points_by_year_pivot[SCORING_TYPE_COL_MATCHING[scoring_type]] = fantasy_points_by_year_pivot[SCORING_TYPE_COL_MATCHING[scoring_type]].round(1)
+        fantasy_points_by_year_pivot[col_to_agg_name] = fantasy_points_by_year_pivot[col_to_agg_name].round(1)
     return fantasy_points_by_year_pivot
 
+#Sidebar for user input
 st.sidebar.header('Season Total Data Settings')
 
 start_year, end_year = st.sidebar.select_slider(
@@ -79,7 +86,11 @@ start_year, end_year = st.sidebar.select_slider(
      options=list(range(2019,2022)),
      value=(2019,2021))
 
+#Aggregation function
 selected_agg_func = st.sidebar.radio('Aggregation Method to Apply',['sum','mean','median'])
+
+#Option to normalize
+selected_to_normalize = st.sidebar.checkbox('Normalize by games played', value=False)
 
 #Allow user to enter a point floor
 point_value_floor = st.sidebar.number_input('Point Floor for Chart Display', value=0, min_value=0)
@@ -89,14 +100,10 @@ selected_scoring_type = st.sidebar.selectbox('Fantasy Point Scoring Style', SCOR
 with st.sidebar.expander("See scoring style details"):
      st.write('feature under construction')
 
-#Allow choice of analysis by total season points OR average points per game
-st.sidebar.header('THIS CURRENTLY NOT DOING ANYTHING. NEED TO DISCUSS OPTIONS TO NORMALIZE.')
-selected_scoring_agg = st.sidebar.radio(
-     "View Points by",
-     ('Average Per Game', 'Season Total'))
-
 #Get a df for charting season totals data based upon user entry
-pivot_df = get_pivot_table(data, start_year, end_year, selected_agg_func, selected_scoring_type)
+column_name_to_chart = get_y(selected_scoring_type, selected_to_normalize)
+
+pivot_df = get_pivot_table(data, start_year, end_year, selected_agg_func, column_name_to_chart)
 
 st.header('Interactive Stripplot')
 st.markdown("""
@@ -109,18 +116,6 @@ with st.expander('Players Already Drafted'):
     players_already_drafted = st.multiselect('Indicate Players Already Taken',
         distinct_player_names)
 
-#Don't think I need this currently
-# def get_y():
-#     "Helper function to get the appropriate score column to display on stripplot y axis"
-#     if selected_scoring_agg == 'Average Per Game':
-#         return SCORING_TYPE_COL_MATCHING[selected_scoring_type] + 'pG'
-#     else:
-#         return SCORING_TYPE_COL_MATCHING[selected_scoring_type]
-
-#Get user selected y value
-#selected_y = get_y()
-selected_y = SCORING_TYPE_COL_MATCHING[selected_scoring_type]
-
 #Construct altair plot to visualize data
 
 #Create a selector object
@@ -128,10 +123,10 @@ strip_plot_selector = alt.selection_multi(empty='all', fields=['Player'])
 
 #Create a stripplot
 #Get the slice of your dataframe to plot
-stripplot_data = pivot_df[(pivot_df['Player'].isin(players_already_drafted) == False) & (pivot_df[selected_y] >= point_value_floor)]
+stripplot_data = pivot_df[(pivot_df['Player'].isin(players_already_drafted) == False) & (pivot_df[column_name_to_chart] >= point_value_floor)]
 
 #Get values to use as the min and max values for the y-axis
-stripplot_y_max = stripplot_data[selected_y].max() + 10
+stripplot_y_max = stripplot_data[column_name_to_chart].max() + 10
 
 stripplot_y_min = point_value_floor
 
@@ -143,13 +138,13 @@ stripplot =  alt.Chart(stripplot_data).mark_circle(size=50).encode(
         axis=alt.Axis(ticks=True, grid=False, labels=False),
         scale=alt.Scale(),
     ),
-    y=alt.Y(f'{selected_y}:Q',
+    y=alt.Y(f'{column_name_to_chart}:Q',
             scale=alt.Scale(
             domain=(stripplot_y_min, stripplot_y_max)),
                 axis=alt.Axis(title=None)),
     color=alt.condition(strip_plot_selector, 'FantPos', alt.value('lightgray'), legend=None),
     #color=alt.condition(interval, 'Origin', alt.value('lightgray')
-    tooltip=['Player', selected_y],
+    tooltip=['Player', column_name_to_chart],
     column=alt.Column(
         'FantPos:N',
         header=alt.Header(
@@ -186,7 +181,7 @@ line = alt.Chart(data).mark_line(
     point=alt.OverlayMarkDef(color="red")
     ).encode(
     x='year:O',
-    y=f'{selected_y}:Q',
+    y=f'{column_name_to_chart}:Q',
     strokeDash='Player',
     #color='Player',
     tooltip='Player'
